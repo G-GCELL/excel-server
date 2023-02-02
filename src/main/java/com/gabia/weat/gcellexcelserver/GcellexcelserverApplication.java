5d8148e0c4c3536f25172035618140862e604413
Endo <twahn@gabia.com>
2023-02-02 17:40:55 +0900

    Initial commit : Spring Boot Application
    엑셀 파일 생성을 수행하는 서버를 스프링 부트
    애플리케이션으로 생성하였음.
    
    application.yml 을 환경 변수로 설정하였고,
    test 를 위한 옵션은 별도로 생성하여 
    인메모리(h2)에서 테스트가 수행될 수 있도록 하였음.

---
+++ src/main/java/com/gabia/weat/gcellexcelserver/GcellexcelserverApplication.java
@@ -0,0 +1,13 @@
+package com.gabia.weat.gcellexcelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcellexcelserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcellexcelserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabia/weat/gcellexcelserver/GcellexcelserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.weat.gcellexcelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcellexcelserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
