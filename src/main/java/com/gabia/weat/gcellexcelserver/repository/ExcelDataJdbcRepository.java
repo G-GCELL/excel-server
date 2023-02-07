95bd8ce72088de31b74561387e5de8baa547bac8
Endo <twahn@gabia.com>
2023-02-07 16:48:51 +0900

    Feat : Entity Jdbc Repository & Csv Parser 구현
    데이터 리스트를 받아 bulk insert를 수행하도록
    리포지토리를 구현하고,
    
    csv 파일을 읽어 데이터 리스트로 반환하는 parser를
    구현하였음.

--- src/main/java/com/gabia/weat/gcellexcelserver/repository/ExcelDataJdbcRepository.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/repository/ExcelDataJdbcRepository.java
@@ -1,7 +1,12 @@
 package com.gabia.weat.gcellexcelserver.repository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +21,24 @@ public class ExcelDataJdbcRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public void insertExcelDataList(List<ExcelData> excelDataList) {
+		jdbcTemplate.batchUpdate(
+			"insert into excel_data (account_id, start_date, end_date, product_code, cost) values (?, ?, ?, ?, ?)",
+			new BatchPreparedStatementSetter() {
+				@Override
+				public void setValues(@NotNull PreparedStatement ps, int index) throws SQLException {
+					ExcelData excelData = excelDataList.get(index);
+					ps.setString(1, excelData.getAccountId());
+					ps.setTimestamp(2, Timestamp.valueOf(excelData.getStartDate()));
+					ps.setTimestamp(3, Timestamp.valueOf(excelData.getEndDate()));
+					ps.setString(4, excelData.getProductCode());
+					ps.setBigDecimal(5, excelData.getCost());
+				}
 
+				@Override
+				public int getBatchSize() {
+					return excelDataList.size();
+				}
+			});
 	}
 
 }
--- src/main/java/com/gabia/weat/gcellexcelserver/util/CsvParser.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/util/CsvParser.java
@@ -1,19 +1,48 @@
 package com.gabia.weat.gcellexcelserver.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import com.gabia.weat.gcellexcelserver.domain.ExcelData;
 
-import lombok.RequiredArgsConstructor;
-
-@Component
-@RequiredArgsConstructor
 public class CsvParser {
 
-	public List<ExcelData> parseToExcelDataList(String csvFilePath) {
-		return null;
+	public static List<ExcelData> parseToExcelDataList(String csvFilePath) throws IOException {
+		File csvFile = new File(csvFilePath);
+		CSVFormat formatter = CSVFormat.EXCEL
+			.builder()
+			.setHeader()
+			.setSkipHeaderRecord(true)
+			.build();
+
+		CSVParser parser = formatter.parse(new BufferedReader(new FileReader(csvFile)));
+
+		return parser.getRecords()
+			.stream()
+			.map(CsvParser::recordToExcelData)
+			.toList();
+	}
+
+	private static LocalDateTime dateTimeParser(String gtime) {
+		return LocalDateTime.parse(gtime.substring(0, gtime.length() - 1));
+	}
+
+	private static ExcelData recordToExcelData(CSVRecord record) {
+		int endo = 200;
+		return ExcelData.createWithoutId(record.get(0),
+			dateTimeParser(record.get(1)),
+			dateTimeParser(record.get(2)),
+			record.get(3),
+			new BigDecimal(record.get(4)));
 	}
 
 }
