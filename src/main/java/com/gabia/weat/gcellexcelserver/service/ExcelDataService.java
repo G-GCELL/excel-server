7a4815fc567387ee6327db2acb1513054f3ffe77
Endo <twahn@gabia.com>
2023-02-09 14:09:12 +0900

    Feat : ExcelDataService 구현
    엑셀 생성 요청에 따라 엑셀 파일을 만들고 스토리지 서버에 저장하는 메서드를 포함한 서비스를 구현하였음.

---
+++ src/main/java/com/gabia/weat/gcellexcelserver/service/ExcelDataService.java
@@ -0,0 +1,32 @@
+package com.gabia.weat.gcellexcelserver.service;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gabia.weat.gcellexcelserver.dto.FileDto.FileCreateRequestDto;
+import com.gabia.weat.gcellexcelserver.dto.ResultSetDto;
+import com.gabia.weat.gcellexcelserver.repository.ExcelDataJdbcRepository;
+import com.gabia.weat.gcellexcelserver.util.ExcelWriter;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ExcelDataService {
+
+	private final ExcelDataJdbcRepository excelDataJdbcRepository;
+	private final MinioService minioService;
+
+	public void createExcelFile(FileCreateRequestDto dto) throws SQLException {
+		File file = new File(Thread.currentThread() + ".xlsx");
+		ResultSetDto result = excelDataJdbcRepository.getResultSet(dto);
+		ExcelWriter.writeExcel(file, result);
+		minioService.uploadFile(file, Thread.currentThread() + ".xlsx");
+		file.delete();
+	}
+
+}
