965893592e975eddc677457b81f5cfb35c568e3d
AnTaeWook <antk7894@naver.com>
2023-02-12 22:26:12 +0900

    Refactor : Excel Data Service 수정
    파일 생성 및 삭제는 필요한 클래스에게 위임하고,
    이를 반영하여 메서드를 수정하였음.

--- src/main/java/com/gabia/weat/gcellexcelserver/service/ExcelDataService.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/service/ExcelDataService.java
@@ -3,13 +3,13 @@ package com.gabia.weat.gcellexcelserver.service;
 import java.io.File;
 import java.sql.SQLException;
 
+import com.gabia.weat.gcellexcelserver.file.writer.ExcelWriter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gabia.weat.gcellexcelserver.dto.FileDto.FileCreateRequestDto;
 import com.gabia.weat.gcellexcelserver.dto.ResultSetDto;
 import com.gabia.weat.gcellexcelserver.repository.ExcelDataJdbcRepository;
-import com.gabia.weat.gcellexcelserver.util.ExcelWriter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,13 +20,12 @@ public class ExcelDataService {
 
 	private final ExcelDataJdbcRepository excelDataJdbcRepository;
 	private final MinioService minioService;
+	private final ExcelWriter excelWriter;
 
 	public void createExcelFile(FileCreateRequestDto dto) throws SQLException {
-		File file = new File(Thread.currentThread().toString());
 		ResultSetDto result = excelDataJdbcRepository.getResultSet(dto);
-		ExcelWriter.writeExcel(file, result);
-		minioService.uploadFile(file, dto.fileName());
-		file.delete();
+		File excelFile = excelWriter.write(result, Thread.currentThread().toString());
+		minioService.uploadFileWithDelete(excelFile, dto.fileName());
 	}
 
 }
