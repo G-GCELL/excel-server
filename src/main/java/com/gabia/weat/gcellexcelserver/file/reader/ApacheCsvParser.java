1dcc96196e56eb91a92119b89094fadf8f900401
Endo <twahn@gabia.com>
2023-02-13 11:03:55 +0900

    Feat : Stream Parser 구현
    대용량 csv 파일을 stream 으로 읽고 데이터베이스에 저장하는 Stream Csv Parser를 구현하였음,
    
    추가로 파싱 외 데이터 삽입 또한 해당 컴포넌트에 위임하였음.

--- src/main/java/com/gabia/weat/gcellexcelserver/file/reader/ApacheCsvParser.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/file/reader/ApacheCsvParser.java
@@ -1,6 +1,8 @@
 package com.gabia.weat.gcellexcelserver.file.reader;
 
 import com.gabia.weat.gcellexcelserver.domain.ExcelData;
+import com.gabia.weat.gcellexcelserver.repository.ExcelDataJdbcRepository;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -11,11 +13,16 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class ApacheCsvParser implements CsvParser {
 
+    private final ExcelDataJdbcRepository excelDataJdbcRepository;
+
     @Override
-    public List<ExcelData> parseToExcelDataList(String csvFilePath) throws IOException {
+    public void insertWithCsv(String csvFilePath) throws IOException {
         File csvFile = new File(csvFilePath);
         CSVFormat formatter = CSVFormat.EXCEL
                 .builder()
@@ -25,10 +32,11 @@ public class ApacheCsvParser implements CsvParser {
 
         CSVParser parser = formatter.parse(new BufferedReader(new FileReader(csvFile)));
 
-        return parser.getRecords()
-                .stream()
-                .map(this::recordToExcelData)
-                .toList();
+        List<ExcelData> excelDataList = parser.getRecords()
+            .stream()
+            .map(this::recordToExcelData)
+            .toList();
+        excelDataJdbcRepository.insertExcelDataList(excelDataList);
     }
 
     private LocalDateTime dateTimeParser(String gtime) {
@@ -36,7 +44,8 @@ public class ApacheCsvParser implements CsvParser {
     }
 
     private ExcelData recordToExcelData(CSVRecord record) {
-        return ExcelData.createWithoutId(record.get(0),
+        return ExcelData.createWithoutId(
+                record.get(0),
                 dateTimeParser(record.get(1)),
                 dateTimeParser(record.get(2)),
                 record.get(3),
--- src/main/java/com/gabia/weat/gcellexcelserver/file/reader/CsvParser.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/file/reader/CsvParser.java
@@ -1,12 +1,9 @@
 package com.gabia.weat.gcellexcelserver.file.reader;
 
-import com.gabia.weat.gcellexcelserver.domain.ExcelData;
-
 import java.io.IOException;
-import java.util.List;
 
 public interface CsvParser {
 
-    List<ExcelData> parseToExcelDataList(String csvFilePath) throws IOException;
+    void insertWithCsv(String csvFilePath) throws IOException;
 
 }
