bcef9e1ca5280ddbd4ce6f28465265af5a45576d
김우용 <wykim@gabia.com>
2023-02-17 15:45:14 +0900

    Refactor : RabbitmqProperty 추가 기존 RabbitmqConfig 클래스의 property 필드들을 묶어 하나의 객체로 관리하도록 수정하였음. yml 파일 rabbitmq 프로퍼티들을 구분하기 쉽도록 수정하였음.

--- src/main/java/com/gabia/weat/gcellexcelserver/GcellExcelServerApplication.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/GcellExcelServerApplication.java
@@ -2,8 +2,10 @@ package com.gabia.weat.gcellexcelserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GcellExcelServerApplication {
 
 	public static void main(String[] args) {
