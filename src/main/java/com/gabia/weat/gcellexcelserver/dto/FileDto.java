830ac2395ef2d461e515c51f727f33bb898eab1a
Endo <twahn@gabia.com>
2023-02-20 17:28:38 +0900

    Feat: Excel Create Request Dto 검증
    엑셀 파일 생성 요청 dto의 값들 검증을 위해
    어노테이션으로 값들의 자격을 추가하였고,
    
    해당 dto를 받는 ExcelDataService에 검증 수행을 위한
    어노테이션을 추가하였음.

--- src/main/java/com/gabia/weat/gcellexcelserver/dto/FileDto.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/dto/FileDto.java
@@ -3,20 +3,36 @@ package com.gabia.weat.gcellexcelserver.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class FileDto {
 
 	public record FileCreateRequestDto(
+		@NotNull
 		Long memberId,
+		@NotBlank
 		String fileName,
 		String[] inAccountId,
 		String[] notAccountId,
 		String[] inProductCode,
 		String[] notProductCode,
+		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		LocalDateTime startDateMin,
+		@NotNull
+		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		LocalDateTime startDateMax,
+		@NotNull
+		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		LocalDateTime endDateMin,
+		@NotNull
+		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		LocalDateTime endDateMax,
+		@NotNull
 		BigDecimal costMin,
+		@NotNull
 		BigDecimal costMax
 	) {
 	}
