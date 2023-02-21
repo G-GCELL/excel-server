63bf4666d12de8c15dc632ea8d0181508546e14c
김우용 <wykim@gabia.com>
2023-02-21 16:34:14 +0900

    Fix : MessageWrapperDto 기본 생성자 추가

--- src/main/java/com/gabia/weat/gcellexcelserver/dto/MessageWrapperDto.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/dto/MessageWrapperDto.java
@@ -2,8 +2,10 @@ package com.gabia.weat.gcellexcelserver.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MessageWrapperDto<T> {
 
