5947bea6e6797794be43e86b0965ad170e9083b0
Endo <twahn@gabia.com>
2023-02-22 10:45:48 +0900

    Refactor: empty list 검사 방식 수정
    size() > 0 으로 검사하는 대신 isEmpty() 를 활용하였음

--- src/main/java/com/gabia/weat/gcellexcelserver/repository/query/StringQueryGenerator.java
+++ src/main/java/com/gabia/weat/gcellexcelserver/repository/query/StringQueryGenerator.java
@@ -37,7 +37,7 @@ public class StringQueryGenerator implements QueryGenerator {
         Map<Integer, Object> paramMap = new HashMap<>();
         int paramOrder = 0;
 
-        if (dto.inAccountId() != null && dto.inAccountId().size() > 0) {
+        if (dto.inAccountId() != null && !dto.inAccountId().isEmpty()) {
             if (dto.inAccountId().size() <= 1) {
                 dto.inAccountId().add("");
             }
@@ -50,7 +50,7 @@ public class StringQueryGenerator implements QueryGenerator {
             conditions.add(stringBuilder.append(")").toString());
         }
 
-        if (dto.notAccountId() != null && dto.notAccountId().size() > 0) {
+        if (dto.notAccountId() != null && !dto.notAccountId().isEmpty()) {
             stringBuilder = new StringBuilder("account_id not in (");
             for (String accountId : dto.notAccountId()) {
                 paramMap.put(++paramOrder, accountId);
@@ -60,7 +60,7 @@ public class StringQueryGenerator implements QueryGenerator {
             conditions.add(stringBuilder.append(")").toString());
         }
 
-        if (dto.inProductCode() != null && dto.inProductCode().size() > 0) {
+        if (dto.inProductCode() != null && !dto.inProductCode().isEmpty()) {
             stringBuilder = new StringBuilder("product_code in (");
             for (String productCode : dto.inProductCode()) {
                 paramMap.put(++paramOrder, productCode);
@@ -70,7 +70,7 @@ public class StringQueryGenerator implements QueryGenerator {
             conditions.add(stringBuilder.append(")").toString());
         }
 
-        if (dto.notProductCode() != null && dto.notProductCode().size() > 0) {
+        if (dto.notProductCode() != null && !dto.notProductCode().isEmpty()) {
             stringBuilder = new StringBuilder("product_code not in (");
             for (String productCode : dto.notProductCode()) {
                 paramMap.put(++paramOrder, productCode);
@@ -99,7 +99,7 @@ public class StringQueryGenerator implements QueryGenerator {
         }
 
         stringBuilder = new StringBuilder();
-        if (conditions.size() > 0) {
+        if (!conditions.isEmpty()) {
             stringBuilder.append(" where ");
             for (String condition : conditions) {
                 stringBuilder.append(condition).append(" and ");
