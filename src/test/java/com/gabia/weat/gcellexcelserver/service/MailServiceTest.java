3ac2b87ea9fed416af23405b7816adf5ffbce97a
Endo <twahn@gabia.com>
2023-03-22 09:20:25 +0900

    Test: MailServiceTest 수정
    mail service의 커버리지를 위해 테스트 코드를 수정하였음.

--- src/test/java/com/gabia/weat/gcellexcelserver/service/MailServiceTest.java
+++ src/test/java/com/gabia/weat/gcellexcelserver/service/MailServiceTest.java
@@ -2,26 +2,43 @@ package com.gabia.weat.gcellexcelserver.service;
 
 import static org.mockito.BDDMockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import jakarta.mail.Address;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
 
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
-	@Mock
 	private MailService mailService;
+	private JavaMailSender javaMailSender;
+	private MimeMessage mimeMessage;
+
+	@BeforeEach
+	public void before() {
+		mimeMessage = new MimeMessage((Session)null);
+		javaMailSender = mock(JavaMailSender.class);
+		when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+		mailService = new MailService(javaMailSender);
+	}
 
 	@Test
 	@DisplayName("메일 서비스를 통해 특정 사용자에게 메일을 전송할 수 있다.")
 	public void sendMailTest() {
 		// given & when
-		mailService.sendMail("test@gabia.com", "testMail", "This is test");
+		mailService.sendMail("test@gabia.com", "title", "message");
 
 		// then
-		then(mailService).should().sendMail(any(), any(), any());
+		then(javaMailSender).should().send(any(MimeMessage.class));
 	}
 
 }
